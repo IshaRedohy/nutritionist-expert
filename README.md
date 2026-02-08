1ff30c908eb9f6498bc2c83721ffa1cde486dca6
Shah Isha Redohy <blackeyes0043@gmail.com>
2026-02-08 02:28:02 -0500

    phase 1 done

---
+++ README.md
@@ -0,0 +1,5 @@
+# A nutrition expert chef AI agent
+
+### Phase-1 (Initiate the prompt and model)
+
+This is a local AI agent powered by [llama3.1](https://ollama.com/library/llama3.1) model and [langchain](https://docs.langchain.com/oss/python/integrations/chat/ollama). You input a list of food ingredients and the model replies with a table of its nutritional values. This is the phase 1 of the project. In next phase I will add documents containing nutrional facts of over thousands of ingredients so that the model can reply more precisely and fastly.
--- dev.py
+++ dev.py
@@ -4,7 +4,10 @@ from langchain_core.prompts import ChatPromptTemplate
 llm = OllamaLLM(model="llama3.1")
 
 template = """
-You are a helpful assistant that translates English to French. Translate the user sentence.
+You are a helpful nutritionist assistant who helps users understand the nutritional value of ingredients provided by the user. 
+You will receive a list of ingredients and provide information about their nutritional content, such as calories, macronutrients 
+(carbohydrates, proteins, fats), vitamins, and minerals. You should provide your response in a table format for easy readability. 
+Don't provide any additional information or explanations, just the nutritional content in a clear and concise manner.
 
 User sentence: {question}
 """
@@ -12,7 +15,9 @@ User sentence: {question}
 prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | llm
 
-q = input("What would you like to translate?\n")
-
-res = chain.invoke({"question": q})
-print(res)
+while True:
+    ingredients = list(input("Enter the ingredients(type bye to quit the agent): \n").lower().split(","))
+    if "bye" in ingredients:
+        break
+    res = chain.invoke({"question": ingredients})
+    print(res)
